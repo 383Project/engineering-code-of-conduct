9e4c975b081427605590eec1a146256ec8d024d1
Estelle Pieragnolo <estellepieragnolo@gmail.com>
2023-05-05 15:19:23 +0100

    add comments

--- operations/work-experience/complete-build/src/quiz.js
+++ operations/work-experience/complete-build/src/quiz.js
@@ -1,12 +1,16 @@
 import quiz from "./data/data.json" assert { type: "json" };
 
+//Initialization of a question count and the score to 0
 let questionsCount = 0;
 let score = 0;
+
+//Store the html elements into variables to use them in the different functions
 const title = document.getElementById("title");
 const buttonContainer = document.getElementById("buttonContainer");
 const questionContainer = document.getElementById("question");
 const answersContainer = document.getElementById("answers");
 
+//Display the first page of the quiz
 function buildQuiz() {
   title.innerText = `Welcome to the Online Quiz Project`;
   buttonContainer.innerHTML = `<button>Let's go!</button>`;
@@ -15,6 +19,7 @@ function buildQuiz() {
   };
 }
 
+//Display the page with the multiple choice question
 function questionPage() {
   title.innerText = `Question ${questionsCount + 1}`;
   buttonContainer.innerHTML = `<button>Submit</button>`;
@@ -22,9 +27,9 @@ function questionPage() {
     showResultPage();
   };
 
+  //Check if there is a question left and display it. If not, call endPage()
   if (questionsCount <= quiz.length - 1) {
     const { question, options } = quiz[questionsCount];
-
     questionContainer.innerHTML = question;
     answersContainer.innerHTML = displayRadio(options);
   } else {
@@ -32,6 +37,7 @@ function questionPage() {
   }
 }
 
+//Map into the answers, format them and return them
 function displayRadio(options) {
   return options
     .map((option) => {
@@ -45,33 +51,40 @@ function displayRadio(options) {
     .join("");
 }
 
+//Display the page with the answer after submitting your answer
 function showResultPage() {
   const answerContainers = answersContainer.querySelectorAll(".answer");
   let answerText = "";
   let answer;
 
+  //Store your answer's id into a variable
   answerContainers.forEach((currenAns) => {
     if (currenAns.checked) {
       answer = currenAns.id;
     }
   });
 
+  //Check if your answer's id is the same as the correct and display a different message if your answer is correct or wrong and update your score
   if (answer === quiz[questionsCount].answer) {
     answerText = `<p id="correct">Correct answer ! </p>`;
+    //Add +1 to your score
     score++;
   } else {
     answerText = `<p id="wrong" >Wrong ! The correct answer was : ${quiz[questionsCount].answer} </p>`;
   }
 
-  buttonContainer.innerHTML = `<button onclick='questionPage()'>Next</button>`;
+  buttonContainer.innerHTML = `<button>Next</button>`;
   buttonContainer.onclick = function () {
     questionPage();
   };
   questionContainer.innerHTML = answerText;
   answersContainer.innerText = ``;
+
+  //Add +1 to the question count
   questionsCount++;
 }
 
+//Display the last page with a different message depending on your score and display your score
 function endPage() {
   let scoreText = "";
   switch (score) {
